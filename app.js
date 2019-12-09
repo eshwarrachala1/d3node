591c2ab056c7ac8b2dca4c5591459f83f584f928
eshwar rachala <eshwar.appdev@gmail.com>
2019-12-08 22:11:28 -0500

    updated

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index').default;
+var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -23,6 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// app.use(app.router);
+// routes.initialize(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
