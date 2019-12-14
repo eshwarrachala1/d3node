d26857e8f5558d1331b0b92edefc693802304be2
eshwar rachala <eshwar.appdev@gmail.com>
2019-12-14 12:19:55 -0500

    updated

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,5 +2,6 @@ d3.select(".jumbotron").style('background-color', 'lime');
 
 $("#report").on('click', function (event) {
     event.preventDefault();
-    alert($('#textInput')[0].value);
+    alert($('#textInput')[0].value.split(/\b\s+/));
 });
+
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var spawn = require("child_process").spawn;
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -9,9 +10,22 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/sankey', function (req, res, next) {
-  res.render('sankey', {
-    title: 'CX Analytics'
+  var options = {
+    args: [
+      req.query.funds, // starting funds
+      req.query.size, // (initial) wager size
+      req.query.count, // wager count — number of wagers per sim
+      req.query.sims // number of simulations
+    ]
+  };
+
+  var pythonProcess = spawn('python', ['/python/d_alembert.py', options]);
+
+  pythonProcess.stdout.on('data', function (data) {
+    if (err) res.send(err);
+    res.send(data.toString());
   });
 });
+
 
 module.exports = router;
