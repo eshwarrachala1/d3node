bc1ceae7a3c6ec92c10442fdf5758cee5f0138e1
eshwar rachala <eshwar.appdev@gmail.com>
2019-12-14 12:37:28 -0500

    updated

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var spawn = require("child_process").spawn;
+var PythonShell = require('python-shell');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -10,21 +10,10 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/sankey', function (req, res, next) {
-  var options = {
-    args: [
-      req.query.funds, // starting funds
-      req.query.size, // (initial) wager size
-      req.query.count, // wager count — number of wagers per sim
-      req.query.sims // number of simulations
-    ]
-  };
-
-  var pythonProcess = spawn('python', ['/python/d_alembert.py', options]);
-
-  pythonProcess.stdout.on('data', function (data) {
-    if (err) res.send(err);
-    res.send(data.toString());
+  res.render('sankey', {
+    title: 'test'
   });
+
 });
 
 
