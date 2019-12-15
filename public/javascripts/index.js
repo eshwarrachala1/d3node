f49cc4220dcc88013272496e1859c3f6dd814878
eshwar rachala <eshwar.appdev@gmail.com>
2019-12-15 09:00:06 -0500

    updated

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,14 +1,97 @@
 (function () {
     // d3.select(".jumbotron").style('background-color', 'lime');
+    var data = [{
+            "name": "Tiger Nixon",
+            "position": "System Architect",
+            "salary": "$3,120",
+            "start_date": "2011/04/25",
+            "office": "Edinburgh",
+            "extn": "5421"
+        },
+        {
+            "name": "Garrett Winters",
+            "position": "Director",
+            "salary": "$5,300",
+            "start_date": "2011/07/25",
+            "office": "Edinburgh",
+            "extn": "8422"
+        },
+        {
+            "name": "Tiger Nixon",
+            "position": "System Architect",
+            "salary": "$3,120",
+            "start_date": "2011/04/25",
+            "office": "Edinburgh",
+            "extn": "5421"
+        },
+        {
+            "name": "Garrett Winters",
+            "position": "Director",
+            "salary": "$5,300",
+            "start_date": "2011/07/25",
+            "office": "Edinburgh",
+            "extn": "8422"
+        },
+        {
+            "name": "Tiger Nixon",
+            "position": "System Architect",
+            "salary": "$3,120",
+            "start_date": "2011/04/25",
+            "office": "Edinburgh",
+            "extn": "5421"
+        },
+        {
+            "name": "Garrett Winters",
+            "position": "Director",
+            "salary": "$5,300",
+            "start_date": "2011/07/25",
+            "office": "Edinburgh",
+            "extn": "8422"
+        },
+        {
+            "name": "Tiger Nixon",
+            "position": "System Architect",
+            "salary": "$3,120",
+            "start_date": "2011/04/25",
+            "office": "Edinburgh",
+            "extn": "5421"
+        },
+        {
+            "name": "Garrett Winters",
+            "position": "Director",
+            "salary": "$5,300",
+            "start_date": "2011/07/25",
+            "office": "Edinburgh",
+            "extn": "8422"
+        }
+    ];
 
     $(document).ready(function () {
-        $('#example').DataTable();
-    });
+        $('#example').DataTable({
+            data: data,
+            columns: [{
+                    data: 'name'
+                },
+                {
+                    data: 'position'
+                },
+                {
+                    data: 'salary'
+                },
+                {
+                    data: 'office'
+                }
+            ]
 
+
+        });
+    });
     $("#report").on('click', function (event) {
         event.preventDefault();
-       
+
+
         // alert($('#textInput')[0].value.split(/\b\s+/));
     });
+
 
 })();
