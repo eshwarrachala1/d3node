1dc207321055a97f2dc36e84bdf8d5832d091b18
eshwar rachala <eshwar.appdev@gmail.com>
2019-12-14 22:03:05 -0500

    updated python integration

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var PythonShell = require('python-shell');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -10,8 +9,26 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/sankey', function (req, res, next) {
+
+  var spawn = require('child_process').spawn,
+    py = spawn('python', ['/public/python/test.py']),
+    data = [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    dataString = '';
+    
+  py.stdout.on('data', function (data) {
+    dataString += data.toString();
+  });
+
+  py.stdout.on('end', function () {
+    console.log('Sum of numbers=', dataString);
+  });
+
+  py.stdin.write(JSON.stringify(data));
+
+  py.stdin.end();
+
   res.render('sankey', {
-    title: 'test'
+    title: JSON.stringify(data)
   });
 
 });
