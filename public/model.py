eb01f219e630b911e6ce6222ed58e694f61c695a
Eshwar Rachala <eshwar.appdev@gmail.com>
2019-12-27 12:31:07 -0500

    updated

--- public/model.py
+++ public/model.py
@@ -14,17 +14,17 @@ def main():
     arg_parser.add_argument('-l', '--language', help='Path to language configuration file', default="lang_store/english.csv")
     arg_parser.add_argument('-i', '--sentence', help='Input sentence to parse', required=True)
     arg_parser.add_argument('-j', '--json_output', help='path to JSON output file', default="output.json")
-    # arg_parser.add_argument('-t', '--thesaurus', help='path to thesaurus file',default=None) #default="thesaurus_store/th_english.dat")
-    # arg_parser.add_argument('-s', '--stopwords', help='path to stopwords file',default=None) #default="stopwords/english.txt")
+    arg_parser.add_argument('-t', '--thesaurus', help='path to thesaurus file',default=None) #default="thesaurus_store/th_english.dat")
+    arg_parser.add_argument('-s', '--stopwords', help='path to stopwords file',default=None) #default="stopwords/english.txt")
 
     args = arg_parser.parse_args()
 
     ln2sql = Ln2sql(
         database_path=args.database,
         language_path=args.language,
-        json_output_path=args.json_output
-        # thesaurus_path=args.thesaurus,
-        # stopwords_path=args.stopwords,
+        json_output_path=args.json_output,
+        thesaurus_path=args.thesaurus,
+        stopwords_path=args.stopwords,
     ).get_query(args.sentence)
 
 if __name__ == '__main__':
