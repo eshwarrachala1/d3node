abbe62691614931ea3dfa8acef814e4e8bbb9dd5
eshwar rachala <eshwar.appdev@gmail.com>
2019-12-27 00:25:06 -0500

    updated

--- public/ln2sql/main.py
+++ public/ln2sql/main.py
@@ -5,10 +5,10 @@ from .ln2sql import Ln2sql
 
 def main():
     arg_parser = argparse.ArgumentParser(description='A Utility to convert Natural Language to SQL query')
-    arg_parser.add_argument('-d', '--database', help='Path to SQL dump file', required=True)
-    arg_parser.add_argument('-l', '--language', help='Path to language configuration file', required=True)
+    arg_parser.add_argument('-d', '--database', help='Path to SQL dump file', default="database_store/city.sql")
+    arg_parser.add_argument('-l', '--language', help='Path to language configuration file', default="lang_store/english.csv")
     arg_parser.add_argument('-i', '--sentence', help='Input sentence to parse', required=True)
-    arg_parser.add_argument('-j', '--json_output', help='path to JSON output file', default=None)
+    arg_parser.add_argument('-j', '--json_output', help='path to JSON output file', default="output.json")
     arg_parser.add_argument('-t', '--thesaurus', help='path to thesaurus file', default=None)
     arg_parser.add_argument('-s', '--stopwords', help='path to stopwords file', default=None)
 
