d9905e037bf4d4eba01aad7a652fc207fd993c9a
Eshwar Rachala <eshwar.appdev@gmail.com>
2019-12-28 01:10:08 -0500

    updated

--- routes/index.js
+++ routes/index.js
@@ -11,15 +11,58 @@ router.get('/', function (req, res, next) {
 
 
 router.get('/NlSQl', function (req, res, next) {
+
+  var sqlite3 = require('sqlite3').verbose();
+  var db = new sqlite3.Database(':memory:');
+
   console.log(req.query.message);
-  res.contentType('json');
-  res.send([{
+
+  let runPy = new Promise(function (success, nosuccess) {
+    const { spawn } = require('child_process');
+    const pyprog = spawn('python', ["public/model.py", "-i Getme city"]);
+
+    pyprog.stdout.on('data', function (data) {
+      success(data);
+    });
+
+    pyprog.stderr.on('data', (data) => {
+      nosuccess(data);
+    });
+  });
+
+  var data = new Array({
     "id": 1,
-    "cityName": "Jacksonville"
-  }]);
+    "cityName": "test"
+  });
+
+  runPy.then(function (fromRunpy) {
+
+    db.serialize(function () {
+      db.run('CREATE TABLE city (id int(11) NOT NULL,cityName varchar(30) NOT NULL)');
+
+      var stmt = db.prepare('INSERT INTO city VALUES (?,?)');
+
+      stmt.run(1, 'Pune');
+      stmt.run(2, 'Hillwood');
+      stmt.run(3, 'San Jose');
+      stmt.run(4, 'The City');
+      stmt.run(5, 'South Park');
+
+      stmt.finalize();
+
+      db.each(fromRunpy.toString(), function (err, row) {
+        console.log(row.id);
+        data.push({ "id": row.id, "cityName": row.cityName });
+      });
+
+    });
+
+    db.close();
+  });
+
+  res.contentType('json');
+  res.send(data);
 });
-
-
 
 router.get('/sankey', function (req, res, next) {
   res.render('sankey', {
