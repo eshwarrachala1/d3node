a5acdb2f42e5d906496e1d7b31c5f1bb199d9d32
Eshwar Rachala <eshwar.appdev@gmail.com>
2019-12-28 19:51:54 -0500

    updated

--- public/ln2sql/database_store/city.sql
+++ public/ln2sql/database_store/city.sql
@@ -1,22 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 4.6.6
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Oct 23, 2017 at 01:17 AM
--- Server version: 5.6.35
--- PHP Version: 5.6.30
-
+-- SQL MODEL 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Database: `heyzot-analytics`
 --
 
@@ -32,17 +17,6 @@ CREATE TABLE `city` (
   `people` bigint null
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `city`
---
-
-INSERT INTO `city` (`id`, `name`,`people`) VALUES
-(1, 'Pune',3000),
-(2, 'Hillwood',5000),
-(3, 'San Jose',4000),
-(4, 'The City',3000),
-(5, 'South Park',2000);
-
 -- --------------------------------------------------------
 
 --
@@ -55,26 +29,6 @@ CREATE TABLE `emp` (
   `cityId` int(11) NOT NULL,
   `score` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `emp`
---
-
-INSERT INTO `emp` (`id`, `name`, `cityId`, `score`) VALUES
-(1, 'Lord Brain', 1, 5),
-(2, 'Matthew', 2, 4),
-(3, 'Ferrero Jeremy', 3, 6),
-(4, 'Bud Light', 4, 4),
-(5, 'Demi Moore', 5, 6),
-(6, 'Woody Allen', 1, 2),
-(7, 'Joaquin Phoenix', 2, 9),
-(8, 'Meg Ryan', 3, 4),
-(9, 'Trey Anastasio', 4, 3),
-(10, 'Marie Osmond', 5, 6);
-
---
--- Indexes for dumped tables
---
 
 --
 -- Indexes for table `city`
@@ -108,7 +62,3 @@ ALTER TABLE `emp`
 --
 ALTER TABLE `emp`
   ADD CONSTRAINT `emp_ibfk_1` FOREIGN KEY (`cityId`) REFERENCES `city` (`id`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
